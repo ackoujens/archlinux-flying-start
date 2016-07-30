fe8827362e9c73b4c4e6fab908718451ec683159
Ackou Jens <ackoujens@gmail.com>
2016-07-30 11:51:35 +0200

    User input is required for install
    When testing the script chose to install on one of my data drives,
    which is not what I want.

--- one-time-setup.sh
+++ one-time-setup.sh
@@ -46,9 +46,18 @@ echo 'DONE
 '
 
 echo '
+All Mounted Volumes
+-------------------'
+lsblk
+echo "Enter the location of your USB-drive (ex: /dev/sdb):"
+read uservolume
+echo '
+'
+
+echo '
 Repartitioning Drive
 --------------------'
-uservolume="/dev/sda"
+#uservolume="/dev/sda"
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | sudo fdisk $uservolume
   o # clear the in memory partition table
   n # new partition
